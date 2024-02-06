8b84838e55fc1d03c202cee522084fb0742e783b
JafRas307@ <jafrulras@gmail.com>
2024-02-06 11:16:25 +0600

    footer logo change

--- src/components/cards/smallCard.js
+++ src/components/cards/smallCard.js
@@ -1,35 +1,38 @@
 import moment from "moment";
 import React from "react";
-import styles from './card.module.css';
+import styles from "./card.module.css";
 
 export const SmallCard = (props) => {
-    return(
+  const { _id, photo, title = "", shortDesc = "", updatedAt } = props || {};
 
-        <>
-            <div className={styles.mainSmallCard} onClick={()=> props.clickHandler(props?._id)}>
-                <img
-                    src={props.photo}
-                    alt="Picture of the date"
-                    width='100%'
-                    style={{height: '230px'}}
-                />
+  return (
+    <>
+      <div
+        className={styles.mainSmallCard}
+        onClick={() => props.clickHandler(_id)}
+      >
+        <img
+          src={photo}
+          alt="Picture of the date"
+          width="100%"
+          style={{ height: "230px" }}
+        />
 
-                <div className={styles.cardDetials}>
-                    <p className={[styles.subtitle , styles.subtitleFont].join(' ')}>
-                        {moment(props?.updatedAt).format("LLL")}
-                    </p>
-                    <h4 className={styles.title}>
-                         {props?.title.length > 30 ? props?.title.substring(0, 30) + '...' : props?.title}
-                    </h4>
+        <div className={styles.cardDetials}>
+          <p className={[styles.subtitle, styles.subtitleFont].join(" ")}>
+            {moment(updatedAt)?.format("LLL")}
+          </p>
+          <h4 className={styles.title}>
+            {title.length > 30 ? title.substring(0, 30) + "..." : title}
+          </h4>
 
-                    <p className={styles.subtitle}>
-                        {props?.shortDesc.length > 60 ? props?.shortDesc.substring(0, 60) + '...' : props?.shortDesc}
-                    </p>
-            
-
-                </div>
-
-            </div>
-        </>
-    )
-}
+          <p className={styles.subtitle}>
+            {shortDesc.length > 60
+              ? shortDesc.substring(0, 60) + "..."
+              : shortDesc}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
