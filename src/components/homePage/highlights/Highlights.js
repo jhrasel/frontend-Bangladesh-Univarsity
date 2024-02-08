cee6ef11b779f2335fc6c66fd83376cf51823e2d
JafRas307@ <jafrulras@gmail.com>
2024-02-08 19:26:51 +0600

    slider content change

--- src/components/homePage/highlights/Highlights.js
+++ src/components/homePage/highlights/Highlights.js
@@ -23,9 +23,6 @@ export default function Highlights() {
             {/* item */}
             <Col lg={12}>
               <Swiper
-                pagination={{
-                  type: "progressbar",
-                }}
                 breakpoints={{
                   320: {
                     slidesPerView: 1.1,
