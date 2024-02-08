07c0d9d041c5ed3aa3be24d2b29606476fa2c012
JafRas307@ <jafrulras@gmail.com>
2024-02-09 03:22:30 +0600

    change banner Slider content

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,85 +1,95 @@
-import Image from 'next/image';
+import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState } from "react";
 import { Button, Spinner } from "react-bootstrap";
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 import { useQuery } from "react-query";
-import slider from '../../assets/image/slider.jpg';
+import slider from "../../assets/image/slider.jpg";
 import ApiRequest from "../../services/api-services";
-import styles from './slider.module.css';
+import styles from "./slider.module.css";
 
 export const Slider = () => {
-    const router = useRouter()
-    const [index, setIndex] = useState(0);
+  const router = useRouter();
+  const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-    const gotoContact = () => {
-        router.push('/contact-us')
-    }
+  const gotoContact = () => {
+    router.push("/contact-us");
+  };
 
-    const gotoNotice = () => {
-        router.push('/notice')
-    }
+  const gotoNotice = () => {
+    router.push("/notice");
+  };
 
-        const { isLoading, data:galleryItem } = useQuery('gallery_item', () => ApiRequest.get('banner/all'))
+  const { isLoading, data: galleryItem } = useQuery("gallery_item", () =>
+    ApiRequest.get("banner/all")
+  );
 
-    const gallery = useMemo(() => { 
-        const arr = [];
-        galleryItem?.data?.data?.map((singleItem) => { 
-            singleItem?.images?.map((image) => { 
-                const obj = {
-                    ...singleItem,
-                    image
-                }
-                arr.push(obj)
-            })
-        })
-        return arr;
-    }, [galleryItem])
+  const gallery = useMemo(() => {
+    const arr = [];
+    galleryItem?.data?.data?.map((singleItem) => {
+      singleItem?.images?.map((image) => {
+        const obj = {
+          ...singleItem,
+          image,
+        };
+        arr.push(obj);
+      });
+    });
+    return arr;
+  }, [galleryItem]);
 
-    return (
-       
-                    <div className={styles.mt}>
-            {
-                isLoading && <div className={styles.preloader}><Spinner animation="grow" /></div>
-            }
-            <Carousel activeIndex={index} onSelect={handleSelect} >
-                {
-                    gallery.map((item, index) => {
-                        return(
-                            <Carousel.Item key={index}>
-                                <div className='slider_overylay'>
-                                    <Image
-                                        loading="lazy"
-                                        unoptimized
-                                        className={styles.Image_Slider}
-                                        src={item?.image?.path ?? slider.src}
-                                        alt="slider"
-                                        width={0}
-                                        height={0}
-                                        style={{ width: '100%', height: '750px' }}
-                                    />
-                                    <div className='slider_lay'></div>
-                                </div>
-                                <Carousel.Caption>
-                                    <div>
-                                        <p className={'sliderTag'}>{item?.title}t</p>
-                                        <h1 className={'headingTitle mblHeading'}>{item?.description} </h1>
-                                        {/* <Buttons
+  return (
+    <div className={styles.mt}>
+      {isLoading && (
+        <div className={styles.preloader}>
+          <Spinner animation="grow" />
+        </div>
+      )}
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {gallery.map((item, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <div className="slider_overylay">
+                <Image
+                  loading="lazy"
+                  unoptimized
+                  className={styles.Image_Slider}
+                  src={item?.image?.path ?? slider.src}
+                  alt="slider"
+                  width={0}
+                  height={0}
+                  style={{ width: "100%", height: "750px" }}
+                />
+                <div className="slider_lay"></div>
+              </div>
+              <Carousel.Caption>
+                <div>
+                  <p className={"sliderTag"}>{item?.title}</p>
+                  <h1 className={"headingTitle mblHeading"}>
+                    {item?.description}
+                  </h1>
+                  {/* <Buttons
                                             text={'Apply Now'}
                                             bgColor={'#EB2A2E'}
                                             color={'#fff'}
                                             click={gotoContact}
                                         /> */}
-                                        <a href={'/contact-us'}>
-                                            <Button className="galleryButton" variant="danger" style={{marginTop: '0px !important'}}>Apply Now</Button>
-                                        </a>
-                                    </div>
-                                </Carousel.Caption>
-                                {/* <Carousel.Caption>
+                  <a href={"/contact-us"}>
+                    <Button
+                      className="galleryButton"
+                      variant="danger"
+                      style={{ marginTop: "0px !important" }}
+                    >
+                      Apply Now
+                    </Button>
+                  </a>
+                </div>
+              </Carousel.Caption>
+              {/* <Carousel.Caption>
                                     <div className={styles.sliderHeading}>
                                         <h1 className={'headingTitle'}>{item?.title}</h1>
                                         <p className={styles.mobiles}>{item?.description.length > 90 ? item?.description.substring(0, 90) + '...' : item?.description}</p>
@@ -92,12 +102,10 @@ export const Slider = () => {
                                         <Button click={gotoNotice} text={'View Notice'} bgColor={'#F7D4D4'}/>{' '}
                                     </div>
                                 </Carousel.Caption> */}
-                            </Carousel.Item>
-                        )
-                    })
-                }
-            </Carousel>
-        </div>
-    );
-}
-
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
--- src/components/slider/sliderWithButton.js
+++ src/components/slider/sliderWithButton.js
@@ -1,51 +1,63 @@
-import { Container } from 'react-bootstrap';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import life from '../../assets/image/life.png';
-import styles from './sliderWithButton.module.css';
+import { Container } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import life from "../../assets/image/life.png";
+import styles from "./sliderWithButton.module.css";
 
-import athletics from '../../assets/image/athletics.png';
-import graduation from '../../assets/image/graduation.png';
-import social from '../../assets/image/social.png';
+import athletics from "../../assets/image/athletics.png";
+import graduation from "../../assets/image/graduation.png";
+import social from "../../assets/image/social.png";
 
 export const SliderWithButton = () => {
-    return(
-        <Container className={styles.mainSlider_with_btn}>
-             <div className={styles.sliderWithButton}>
-            <Row>
-                <Col xs='12' md='6' lg="3" className='item_middle'>
-                    <div className={styles.innerSliderWithButton} style={{border: '1px solid transparent'}}>
-                        <img src={life.src} alt={'slider image'}/>
-                        <p>University Life</p>
-                        <span>Explore Your Campus Adventure.</span>
-                    </div>
-                </Col>
-    
-                <Col xs='12' md='6' lg="3" className='item_middle' >
-                    <div className={styles.innerSliderWithButton} style={{border: '1px solid transparent'}}>
-                        <img src={graduation.src} alt={'slider image'}/>
+  return (
+    <Container className={styles.mainSlider_with_btn}>
+      <div className={styles.sliderWithButton}>
+        <Row>
+          <Col xs="12" md="6" lg="3" className="item_middle">
+            <div
+              className={styles.innerSliderWithButton}
+              style={{ border: "1px solid transparent" }}
+            >
+              <img src={life.src} alt={"slider image"} />
+              <p>University Life</p>
+              <span>Explore Your Campus Adventure.</span>
+            </div>
+          </Col>
 
-                        <p>Graduation</p>
-                        <span>Your Academic Milestone</span>
-                    </div>
-                </Col>
-                <Col xs='12' md='6' lg="3" className='item_middle'>
-                    <div className={styles.innerSliderWithButton}  style={{border: '1px solid transparent'}}>
-                        <img src={athletics.src} alt={'slider image'}/>
-                        <p>Athletics</p>
-                        <span>Where Champions Are Forged</span>
-                    </div>
-                </Col>
-                <Col xs='12' md='6' lg="3" className='item_middle' >
-                    <div className={styles.innerSliderWithButton} style={{border: '1px solid transparent'}}>
-                        <img src={social.src} alt={'slider image'}/>
+          <Col xs="12" md="6" lg="3" className="item_middle">
+            <div
+              className={styles.innerSliderWithButton}
+              style={{ border: "1px solid transparent" }}
+            >
+              <img src={graduation.src} alt={"slider image"} />
 
-                        <p>Social</p>
-                        <span>Unite, Engage, Thrive</span>
-                    </div>
-                </Col>
-            </Row>
-        </div>
-       </Container>
-    )
-}
+              <p>Graduation</p>
+              <span>Your Academic Milestone</span>
+            </div>
+          </Col>
+          <Col xs="12" md="6" lg="3" className="item_middle">
+            <div
+              className={styles.innerSliderWithButton}
+              style={{ border: "1px solid transparent" }}
+            >
+              <img src={athletics.src} alt={"slider image"} />
+              <p>Our Sports</p>
+              <span>Where Champions Are Forged</span>
+            </div>
+          </Col>
+          <Col xs="12" md="6" lg="3" className="item_middle">
+            <div
+              className={styles.innerSliderWithButton}
+              style={{ border: "1px solid transparent" }}
+            >
+              <img src={social.src} alt={"slider image"} />
+
+              <p>Social</p>
+              <span>Unite, Engage, Thrive</span>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
+};
