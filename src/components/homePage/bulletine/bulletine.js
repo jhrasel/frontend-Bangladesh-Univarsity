80e92d183e1a5063355c41833b7679370c5f31af
JafRas307@ <jafrulras@gmail.com>
2024-02-09 22:59:08 +0600

    view more button added

--- src/components/homePage/bulletine/bulletine.js
+++ src/components/homePage/bulletine/bulletine.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
@@ -7,53 +8,64 @@ import ApiRequest from "../../../services/api-services";
 import { SmallCard } from "../../cards/smallCard";
 import { BulletinePreloader } from "../bulletine/bulletineLoader";
 
-export const Bulletine = ({title}) => {
-    const Router = useRouter()
-    const { isLoading, data:bulletin } = useQuery('bulletin', () => ApiRequest.get('news/all?page=1&perPage=3'))
+export const Bulletine = ({ title }) => {
+  const Router = useRouter();
+  const { isLoading, data: bulletin } = useQuery("bulletin", () =>
+    ApiRequest.get("news/all?page=1&perPage=3")
+  );
 
-    const routerHandler = (_id) => {
-        // Router.push({
-        //     pathname: `news/${_id}`
-        // })
-    }
+  const routerHandler = (_id) => {
+    // Router.push({
+    //     pathname: `news/${_id}`
+    // })
+  };
 
-    
-  return(
-      <div>
-              <div className={'mt_30'}>
-                <Container>
-                    <Row>
-
-                      <h1 className={'sectionHeading'}>{title}</h1>
-                        {
-                            isLoading && (
-                                <>
-                                    <Col  xs='12' md='12' lg="4">
-                                        <BulletinePreloader />
-                                    </Col>
-                                    <Col  xs='12' md='12' lg="4">
-                                        <BulletinePreloader />
-                                    </Col>
-                                    <Col  xs='12' md='12' lg="4">
-                                        <BulletinePreloader />
-                                    </Col>
-                                </>
-                            )
-                        }
-                        {
-                            bulletin?.data?.data?.length && bulletin?.data?.data?.map((value, i) => (
-                                <Col  xs='12' md='12' lg="4" style={{cursor: "pointer"}} key={i} className="bulletine">
-                                    <a href={`/news/${value?._id}`} >
-                                        <SmallCard {...value}  showDate={false} clickHandler={routerHandler}/>
-                                    </a>
-                                </Col>
-                            ))
-                        }
-
-                    </Row>
-                    </Container>
+  return (
+    <div>
+      <div className={"mt_30"}>
+        <Container>
+          <Row>
+            <h1 className={"sectionHeading"}>{title}</h1>
+            {isLoading && (
+              <>
+                <Col xs="12" md="12" lg="4">
+                  <BulletinePreloader />
+                </Col>
+                <Col xs="12" md="12" lg="4">
+                  <BulletinePreloader />
+                </Col>
+                <Col xs="12" md="12" lg="4">
+                  <BulletinePreloader />
+                </Col>
+              </>
+            )}
+            {bulletin?.data?.data?.length &&
+              bulletin?.data?.data?.map((value, i) => (
+                <Col
+                  xs="12"
+                  md="12"
+                  lg="4"
+                  style={{ cursor: "pointer" }}
+                  key={i}
+                  className="bulletine"
+                >
+                  <a href={`/news/${value?._id}`}>
+                    <SmallCard
+                      {...value}
+                      showDate={false}
+                      clickHandler={routerHandler}
+                    />
+                  </a>
+                </Col>
+              ))}
+            <Col lg={12}>
+              <div className="news__see__more">
+                <Link href="/news">See More</Link>
               </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
-
-  )
-}
+    </div>
+  );
+};
